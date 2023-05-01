98e8870397997d85ee926fb220d89baa3f56e07f
JuWeDe <RomaN18011998@gmail.com>
2023-05-01 14:15:39 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'cards';
+  products: Product[] = [
+    {
+      title: 'Product 1',
+      description: 'Some description about product 1',
+      image: './assets/images/pepePunch.png'
+    }
+    ,
+    {
+      title: 'Product 2',
+      description: 'Some description about product 2',
+      image: './assets/images/pepePunch.png'
+    },
+    {
+      title: 'Product 3',
+      description: 'Some description about product 3',
+      image: './assets/images/basedPepe.png'
+    }
+  ];
+  title: any;
 }
+interface Product {
+  title: string;
+  description: string;
+  image: string;
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { CardComponent } from './components/card/card.component';
+import {MatCardModule} from '@angular/material/card';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CardComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
