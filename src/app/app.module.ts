81780097fb3f684e8b22f4600da50845c51f989e
roman <roman18011998@gmail.com>
2024-04-19 19:56:54 +0300

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { AppComponent } from './app.component';
-import { CardComponent } from './components/card/card.component';
 import { AuthDialogComponent } from './components/auth-dialog/auth-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -16,28 +15,26 @@ import { SubsidiesComponent } from './components/subsidies/subsidies.component';
 import {AppRoutingModule} from './app-routing.module'
 import { ButtonPanelComponent } from './components/button-panel/button-panel.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { FlightSearchFormComponent } from './components/flight-search-form/flight-search-form.component';
 import { InfoComponent } from './components/info/info.component';
 import { RoutesComponent } from './components/routes/routes.component';
-import { FlightsComponent } from './components/flights/flights.component'
+import { FlightsComponent } from './components/flights/flights.component';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent,
     AuthDialogComponent,
     HeaderComponent,
     RegistrationPopupComponent,
     SubsidiesComponent,
     ButtonPanelComponent,
     PageNotFoundComponent,
-    FlightSearchFormComponent,
     InfoComponent,
     RoutesComponent,
     FlightsComponent
   ],
   imports: [
-    // NgxDatesPickerModule,
+    MatDatepickerModule,
     AppRoutingModule,
     MatIconModule,
     MatDatepickerModule,
@@ -48,7 +45,9 @@ import { FlightsComponent } from './components/flights/flights.component'
     MatButtonModule,
     MatDialogModule,
   ],
-  providers: [],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'ru-RU' },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/flights/flights.component.ts
+++ src/app/components/flights/flights.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-flights',
